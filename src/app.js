1f1f3f0dd3958eeca503dcf7947d42e62d0936d3
vineetk130699 <vineet@urbanstop.co>
2021-04-28 14:12:11 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+const { rawListeners } = require("process");
+
+const app = express();
+const publicDirectoryPath = path.join(__dirname,"../public");
+const viewsPath = path.join(__dirname,"../templates/views");
+const partialsPath = path.join(__dirname,"../templates/partials");
+
+// console.log(path.join(__dirname,"./public"));
+// console.log(publicDirectoryPath);
+
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+app.use(express.static(publicDirectoryPath));
+
+// handlebars examples
+app.get("",(req,res) => {
+    res.render("index",{
+        by:"Vineet"
+    }); 
+});
+
+app.get('/about', (req,res) => {
+    res.render("about",{
+        name:"Vineet K"
+    });
+})
+
+// API requests and responses examples
+app.get('/products', (req,res) => {
+    // console.log(req.query);
+    res.send({
+        products:[]
+    })
+})
+
+app.get("/weather", (req, res) => {
+    if(!req.query.address){
+        return res.send({
+            error:"Please provide an address"
+        })
+    }
+    
+    res.send({
+        forecast:"It is snowing",
+        address:req.query.address
+    })
+})
+
+// 404 route example
+app.get("*", (req, res) => {
+    res.send("404: Page not found");
+})
+
+app.listen(3000, () => {
+    console.log("Server up and running on port 3000");
+})
