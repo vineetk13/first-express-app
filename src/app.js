49af339cfadd92b0c8b3cfd9e30dfddd1aea40e1
vineetk130699 <vineet@urbanstop.co>
2021-04-30 13:56:38 +0530

    deploy ready on heroku

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const hbs = require("hbs");
 const { rawListeners } = require("process");
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 const publicDirectoryPath = path.join(__dirname,"../public");
 const viewsPath = path.join(__dirname,"../templates/views");
 const partialsPath = path.join(__dirname,"../templates/partials");
@@ -56,6 +58,6 @@ app.get("*", (req, res) => {
     res.send("404: Page not found");
 })
 
-app.listen(3000, () => {
-    console.log("Server up and running on port 3000");
+app.listen(port, () => {
+    console.log("Server up and running on port " + port);
 })
